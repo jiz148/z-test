d68c6f895bca3ec97fd5adbfb663cd6c3817e69a
jiz148 <jizjiz148148@gmail.com>
2021-11-17 21:48:49 -0500

    modified main

--- common/z_table.py
+++ common/z_table.py
@@ -14,7 +14,7 @@ def print_normal_distribution():
     fig, ax = plt.subplots(figsize=(10, 5))
     ax.plot(x, pdf_normal_distribution)
     ax.set_ylim(0)
-    ax.set_title('Normal Distribution', size=20)
+    ax.set_title('Standardized Normal Distribution', size=20)
     ax.set_ylabel('Probability Density', size=20)
     plt.show()
 
@@ -25,14 +25,36 @@ def normal_p_density(x):
 
 
 def calculate_p_from_z(z):
+    """
+    two tails
+    """
     p1, _ = quad(normal_p_density, np.NINF, -z)
     p2, _ = quad(normal_p_density, z, np.Inf)
     return p1 + p2
 
 
+def get_z_table():
+    std_normal_table = pd.DataFrame(data=[],
+                                    index=np.round(np.arange(0, 3.5, .1), 2),
+                                    columns=np.round(np.arange(0.00, .1, 0.01), 2)
+                                    )
+    for i in std_normal_table.index:
+        for c in std_normal_table.columns:
+            z = np.round(i + c, 2)
+            value, _ = quad(normal_p_density, np.NINF, z)
+            std_normal_table.loc[i, c] = value
+
+    std_normal_table.index = std_normal_table.index.astype(str)
+    std_normal_table.columns = [str(column).ljust(4, '0') for column in std_normal_table.columns]
+    return std_normal_table
+
+
 if __name__ == "__main__":
-    print_normal_distribution()
+    # print z distribution
+    # print_normal_distribution()
 
     # calculate cumulative distribution
-    print(calculate_p_from_z(3))
+    # print(calculate_p_from_z(3), '\n')
 
+    # print z-table for approximating  p-value by hand
+    print(get_z_table().to_markdown())
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     mu = 40
     ages_list = [40, 80, 43, 28, 17, 74] * 30
     ages_list_2 = [100, 104, 85, 99, 150, 200] * 30
-    ages = np.array(ages_list)
+    ages = np.array(ages_list_2)
 
     # calculate z-statistic
     avg = np.average(ages)
